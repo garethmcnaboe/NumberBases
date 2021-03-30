4cb401727d5588339639730f9954dcd05165ba87
Gareth McNaboe <gareth.mcnaboe.2021@mumail.com>
2021-03-30 11:17:48 +0100

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,50 +8,54 @@ public class Main {
         int numPalindromes = 0;
 
         for (int i = 2; i < 11; i++) {
-            String result = "";
+            int result = 0;
             int remainder = 0;
             int numToCheck = input;
+            double x = 0;
             while(numToCheck>0){
                 remainder = numToCheck % i;
-                System.out.println(remainder);
+                remainder = (int) (remainder * Math.pow(10.0,x));
                 result = result + remainder;
                 numToCheck = numToCheck/i;
+                x++;
             }
-            String resultReversed = reverseString(result);
-            boolean isPallindrome = checkPalindrome(resultReversed);
+            System.out.println(result);
+            boolean isPallindrome = checkPalindrome(result);
             if(isPallindrome){
                 numPalindromes++;
             }
         }
         return numPalindromes;
     }
-    //by-pass this method if timing issues arise
-    public static String reverseString(String result){
-        String resultReversed = "";
 
-        for(int i = result.length()-1; i>-1; i--){
-            resultReversed = resultReversed + result.charAt(i);
+    public static boolean checkPalindrome(int result) {
+        boolean isPallindrome = true;
+        //method to count the number of digits in the number
+        int counter = 0;
+        int checker = result;
+        while(checker>0){
+            checker = checker / 10;
+            counter++;
         }
-        return resultReversed;
-    }
+        //create an array to store the digits
+        int[] newArray = new int[counter];
+        //place each individual digit into the array
+        for(int i = 0; i<counter; i++){
+            newArray[i]=result%10;
+            result = result/10;
+        }
 
-    public static boolean checkPalindrome(String input) {
-        int length = input.length();
-        int a = 0;
-        int z = length - 1;
-        int m = length / 2;
+        int length = counter;
+        counter = counter -1;
 
-        boolean pallindrome = true;
-
-        while (pallindrome && (z > m)) {
-            if (input.charAt(a) == input.charAt(z)) {
-                a++;
-                z--;
-            } else {
-                pallindrome = false;
+        for(int j = 0; j<(newArray.length/2); j++){
+            System.out.println(newArray[j]+" "+newArray[counter]);
+            if(newArray[j] != newArray[counter]){
+                isPallindrome = false;
             }
+            counter = counter -1;
         }
-        return pallindrome;
+        return isPallindrome;
     }
 
     public static void main(String[] args) {
@@ -62,6 +66,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         numToCheck = sc.nextInt();
         numPalindromes = checkNumber(numToCheck);
-        System.out.println(numPalindromes);
+        System.out.println("Number of Pallindromes " + numPalindromes);
     }
 }
